bae7c929b22fc1d88da08310b0152ed7c6f0d74d
madan1988 <madan.ase@gmail.com>
2013-10-06 12:49:36 +0530

    Initial Commit

---
+++ DS/Graph.java
@@ -0,0 +1,65 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class Graph {
+	
+	private Map<Integer, LinkedList> adjList;
+	
+	public Graph()
+	{
+		adjList = new HashMap<Integer,LinkedList>();
+	}
+	
+	private void addVertex(int vertex)
+	{
+		if(adjList != null)
+		{
+			adjList.put(vertex, new LinkedList());
+		}
+		
+	}
+	
+	private void addEdge(int vertex, int edge)
+	{
+		if(adjList != null)
+		{
+			adjList.get(vertex).add(edge);
+		}
+	}
+	
+	private void printAdjacencyList()
+	{
+		Iterator it = adjList.keySet().iterator();
+	}
+	
+	public static void main(String[] args) {
+		
+		Graph g = new Graph();
+		g.addVertex(1);
+		g.addVertex(2);
+		g.addVertex(3);
+		g.addVertex(4);
+		g.addVertex(5);
+		g.addEdge(1, 2);
+		g.addEdge(1,5);
+		g.addEdge(2,1);
+		g.addEdge(2,5);
+		g.addEdge(2,3);
+		g.addEdge(2,4);
+		g.addEdge(3,2);
+		g.addEdge(3,4);
+		g.addEdge(4,2);
+		g.addEdge(4,5);
+		g.addEdge(4,3);
+		g.addEdge(5,4);
+		g.addEdge(5,1);
+		g.addEdge(5,2);
+		
+		
+		
+		
+		
+	}
+}
